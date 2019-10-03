bd3163e65e2275bcedfcce41c79b0df1b20e232e
Ana Bastos <analpbastos@gmail.com>
2019-10-03 01:57:59 -0300

    Unnecessarily adding ramda because i want to

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 #!/usr/bin/env node
-
 const exect = require('child_process').exec;
 const path = require('path');
 const fs = require('fs');
+
+const R = require('ramda');
 
 const mainPath = path.dirname(fs.realpathSync(__filename));
 const soundPath = path.join(mainPath, './daledale');
 
 const daledale = function (){
-    const linuxcmd = 'paplay '+soundPath+'.ogg';
-    const windowscmd = path.join(mainPath, './forWindows.vbs')+' '+soundPath+'.mp3';
-    const maccmd = 'afplay '+soundPath+'.mp3';
+    const linuxcmd = R.join('', ['paplay ', soundPath, '.ogg']);
+    const windowscmd = R.join('', [path.join(mainPath, './forWindows.vbs'), ' ', soundPath, '.mp3']);
+    const maccmd = R.join('', ['afplay ', soundPath, '.mp3']);
 
     const platform = process.platform;
 
-    if(platform === 'linux'){
-        return exec(linuxcmd);
-    }
-    else if(platform === 'win32'){
-        return exec(windowscmd);
-    } else if(platform === 'darwin'){
-        return exec(maccmd);
-    }
+    R.cond([
+        [R.equals('linux'), exec(linuxcmd)],
+        [R.equals('win32'), exec(windowscmd)],
+        [R.equals('darwin'), exec(maccmd)],
+    ], platform)
 
     function exec(cmd){
-        return exect(cmd, function (error, stdout, stderr) {
-           if(error)
-               console.error(error);
+        return exect(cmd, function (error) {
+            R.ifElse(
+               R.empty,
+               () => console.log('VAMO DALE!'),
+               (error) => console.error(error),
+               error)
         });
     }
 }
